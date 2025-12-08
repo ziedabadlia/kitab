518ed41d2dbb5b61e2a2f22898609f37cbc8f007
Ziad Abadlia <ziadabadlia@gmail.com>
2025-12-08 21:54:39 +0100

    feat/setted up nextjs app + main tailwind configuration

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h3 className='text-3xl text-emerald-500 font-bold'>Home page</h3>
+    </div>
+  );
+}
