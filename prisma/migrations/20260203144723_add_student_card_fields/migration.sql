99e73ea2c6490a9a7f5e40f1aafad2f7386eb1d8
Ziad Abadlia <ziadabadlia@gmail.com>
2026-02-03 15:47:37 +0100

    feat: add additional fields to Student model for date of birth, department, contact number, and address

---
+++ prisma/migrations/20260203144723_add_student_card_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "students" ADD COLUMN     "address" TEXT,
+ADD COLUMN     "contact_no" TEXT,
+ADD COLUMN     "date_of_birth" TIMESTAMP(3),
+ADD COLUMN     "department" TEXT;
